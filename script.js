4e56820f14e47137adc8a3e2174c4b79d771ffa7
Farzaneh Haghani <68525882+farzaneh-haghani@users.noreply.github.com>
2022-07-27 11:05:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-var srcs;
-
-
-srcs = ['https://th.bing.com/th/id/OIP.ZmR9c4tJ-4ERclMj-Ni3VQHaEg?w=269&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7', 'https://th.bing.com/th/id/OIP.dZ0Qq2oKBoVvONzSfdGsAAHaE8?w=277&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7', 'https://th.bing.com/th/id/OIP.aqSVE7TUcbsLCQdF_FPfRgHaEK?w=307&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7', 'https://th.bing.com/th/id/OIP.1YM53mG10H_U25iPjop83QHaEo?w=264&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7', 'https://th.bing.com/th/id/OIP.PYipJ_hSncugM2SwnZitvgHaEK?w=293&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7'];
-
-
-document.getElementById('next').addEventListener('click', (event) => {
-  srcs.push(srcs.shift());
-  let element_image = document.getElementById('image');
-  element_image.setAttribute("src", srcs[0]);
-
-});
-
-document.getElementById('previous').addEventListener('click', (event) => {
-  srcs.unshift(srcs.slice(-1)[0]);
-  let element_image2 = document.getElementById('image');
-  element_image2.setAttribute("src", srcs.pop());
-
-});
-
 var x, shopping_list, count, j, i, k, item;
 
 // Describe this function...
